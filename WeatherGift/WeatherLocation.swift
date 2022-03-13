80ce5a0cb77333ebff59e563dbfcc0ba329f9b3b
Daniel Park <parkant@bc.edu>
2022-03-13 13:45:51 -0400

    with .gitignore

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Danny Park on 3/13/22.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
